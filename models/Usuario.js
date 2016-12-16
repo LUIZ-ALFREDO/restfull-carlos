b227ecbf3e19931a7085e3a384f54b3f79f0cbee
LUIZ-ALFREDO <lalvieira@gmail.com>
2016-12-16 09:13:25 -0200

    teste
    safdsa

---
+++ models/Usuario.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var UsuarioSchema = new mongoose.Schema({
+  nome: String,
+  idade: Number,
+  perfil:{
+  	 nome:String
+  },
+  cep:Number,
+  endereco:String,
+  login:String,
+  senha:String
+});
+
+module.exports = mongoose.model('Usuario', UsuarioSchema);
